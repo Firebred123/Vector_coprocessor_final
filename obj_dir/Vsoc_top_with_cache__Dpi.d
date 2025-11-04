54827f8ca17ff7cde9f4b2346ca65d1ff438b32c
Firebred123 <tarushgaarg@gmail.com>
2025-11-04 01:28:15 +0000

    Stop tracking obj folder

--- obj_dir/Vsoc_top_with_cache__Dpi.d
+++
@@ -1,7 +0,0 @@
-Vsoc_top_with_cache__Dpi.o: Vsoc_top_with_cache__Dpi.cpp \
- Vsoc_top_with_cache__Dpi.h \
- /opt/verilator/share/verilator/include/vltstd/svdpi.h \
- Vsoc_top_with_cache.h \
- /opt/verilator/share/verilator/include/verilated_heavy.h \
- /opt/verilator/share/verilator/include/verilated.h \
- /opt/verilator/share/verilator/include/verilatedos.h
--- obj_dir/verilated_vcd_c.d
+++
@@ -1,9 +0,0 @@
-verilated_vcd_c.o: \
- /opt/verilator/share/verilator/include/verilated_vcd_c.cpp \
- /opt/verilator/share/verilator/include/verilatedos.h \
- /opt/verilator/share/verilator/include/verilated.h \
- /opt/verilator/share/verilator/include/verilated_vcd_c.h \
- /opt/verilator/share/verilator/include/verilated_trace.h \
- /opt/verilator/share/verilator/include/verilated_trace_defs.h \
- /opt/verilator/share/verilator/include/verilated_trace_imp.cpp \
- /opt/verilator/share/verilator/include/verilated_intrinsics.h
